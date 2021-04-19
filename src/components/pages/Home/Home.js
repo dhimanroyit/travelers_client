c85975830dad32e1907b577ae76abfb66507882d
dhimanroyit <dhimanroyict@gmail.com>
2021-04-19 21:29:44 +0600

    import special service into home page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../../Hero/Hero';
 import ServiceSection from '../../ServiceSection/ServiceSection';
+import ServiceSpacial from '../../ServiceSpacial/ServiceSpacial';
 
 function Home() {
   return (
     <>
       <Hero />
       <ServiceSection />
+      <ServiceSpacial />
     </>
   )
 }
