325301b9a084bd088840d22e4c222a9d79ec797b
dhimanroyit <dhimanroyict@gmail.com>
2021-04-20 20:01:06 +0600

    make sidebar menu item update

--- src/components/SideBar/SideBarMenuItem/SideBarMenuItem.js
+++ src/components/SideBar/SideBarMenuItem/SideBarMenuItem.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function SideBarMenuItem({path, title, icon}) {
   return (
-      <Link className="px-10 py-2 block text-white text-sm hover:bg-gray-600 transition-colors duration-100" to={path}>
+      <Link className="px-10 py-2 block text-white capitalize text-sm hover:bg-gray-600 transition-colors duration-100" to={path}>
         <span className="mr-2 text-xs"><FontAwesomeIcon icon={icon} /></span>
         {title}
       </Link>
