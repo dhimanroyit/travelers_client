f69816d72b88fff99ce79af2a375bfb34e8b372f
dhimanroyit <dhimanroyict@gmail.com>
2021-04-21 23:38:51 +0600

    connect to server data into manage service

--- src/components/ManageServices/ManageServices.js
+++ src/components/ManageServices/ManageServices.js
@@ -1,7 +1,24 @@
-import React from 'react'
-import ManageServiceItem from './ManageServiceItem/ManageServiceItem'
+import React, {useState, useEffect} from 'react'
+import ManageServiceItem from './ManageServiceItem/ManageServiceItem';
+import axios from 'axios';
 
 function ManageServices() {
+  const [service, setService] = useState(null);
+  useEffect(() => {
+    axios.get('/services')
+    .then(service => {
+      const {data} = service;
+      setService(data);
+    })
+  }, [service])
+  const deleteProductHandler = (id) => {
+    axios.delete(`/service/${id}`)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <section className="bg-white rounded-xl p-7 m-6">
       <div className="px-5 py-1.5 bg-gray-200 rounded-lg grid grid-cols-3">
@@ -10,8 +27,15 @@ function ManageServices() {
         <h3>Action</h3>
       </div>
       <div className="mt-3 space-y-3">
-        <ManageServiceItem title="dalsdfa" description="lalsdkfjalkdsf" />
-        <ManageServiceItem title="dalsdfa" description="lalsdkfjalkdsf" />
+        {service && service.map(service => (
+          <ManageServiceItem 
+            key={service._id}
+            id={service._id}
+            title={service.serviceName} 
+            description={service.description} 
+            deleteService={deleteProductHandler} />
+
+        ))}
       </div>
     </section>
   )
