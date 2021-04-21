33e5b381911846109421682a9267720c8f5f31b0
dhimanroyit <dhimanroyict@gmail.com>
2021-04-21 11:38:09 +0600

    create login context

---
+++ src/context/loginContext.js
@@ -0,0 +1,26 @@
+import {createContext, useState} from 'react';
+
+export const loginContext = createContext();
+
+const LoginContextProvider = ({children}) => {
+  const {loginUser, setLoginUser} = useState({
+    login: false,
+    isAdmin: false,
+    name: "",
+    email: "",
+    photo: "",
+  });
+
+const loginData = {
+  loginUser,
+  setLoginUser
+}
+
+  return (
+    <loginContext.Provider value={loginData}>
+      {children}
+    </loginContext.Provider>
+  )
+}
+
+export default LoginContextProvider;
